748aee0965550009fcc91025ec199c38b99d8279
NathanHaigh <nathan2.haigh@live.uwe.ac.uk>
2024-10-09 13:16:11 +0100

    Rendering Successful

--- src/Game.cpp
+++ src/Game.cpp
@@ -20,24 +20,72 @@ bool Game::init()
 {
   character = new sf::Sprite;
   passport = new sf::Sprite;
+
+  //Loading Animal Textures
+  if (!animals[0].loadFromFile("../Data/Images/kenney_animalpackredux/PNG/Round/elephant.png"))
+  {
+	  std::cout << "Texture animals[0] didn't load in \n";
+  }
+
+  if (!animals[1].loadFromFile("../Data/Images/kenney_animalpackredux/PNG/Round/moose.png"))
+  {
+	  std::cout << "Texture animals[1] didn't load in \n";
+  }
+
+  if (!animals[2].loadFromFile("../Data/Images/kenney_animalpackredux/PNG/Round/penguin.png"))
+  {
+	  std::cout << " Texture animals[2] didn't load in \n";
+  }
   
-  if (!background_texture.loadFromFile("Data/Images/kenney_physicspack/PNG/Backgrounds/colored_land.png"))
+  //
+  //Loading Passport Textures
+  if (!passports[0].loadFromFile("../Data/Images/Critter Crossing Customs/elephant passport.png"))
+  {
+	  std::cout << "Texture passport[0] didn't load in \n";
+  }
+
+  if (!passports[1].loadFromFile("../Data/Images/Critter Crossing Customs/moose passport.png"))
+  {
+	  std::cout << "Texture passport[1] didn't load in \n";
+  }
+
+  if (!passports[2].loadFromFile("../Data/Images/Critter Crossing Customs/penguin passport.png"))
+  {
+	  std::cout << " Texture passport[2] didn't load in \n";
+  }
+  //
+  //Loading Backgorund Textures
+  if (!background_texture.loadFromFile("../Data/Images/kenney_physicspack/PNG/Backgrounds/blue_desert.png"))
   {
 	  std::cout << "Background didn't load in \n";
   }
   background.setTexture(background_texture);
 
+  //Background set up that scales to window width
+  auto TextureSize = background_texture.getSize(); // Get size of
+  // texture.
+  const sf::Vector2u& WindowSize = window.getSize(); // Get size of window.
+
+  float ScaleX = (float)WindowSize.x / float(TextureSize.x);
+  float ScaleY = (float)WindowSize.y / float(TextureSize.y);     //Calculate scale.
+
+  background.setTexture(background_texture);
+  background.setScale(ScaleX, ScaleY);
+
+  newAnimal();
   return true;
 }
 
 void Game::update(float dt)
 {
-
+	
 }
 
 void Game::render()
 {
 	window.draw(background);
+	window.draw(*character);
+	window.draw(*passport);
 }
 
 void Game::mouseClicked(sf::Event event)
@@ -74,7 +122,7 @@ void Game::newAnimal()
 	character->setScale(1.8, 1.8);
 	character->setPosition(window.getSize().x / 2, window.getSize().y / 12);
 
-	passport->setTexture(animals[animal_index], true);
+	passport->setTexture(passports[passport_index], true);
 	passport->setScale(0.6, 0.6);
 	passport->setPosition(window.getSize().x / 2, window.getSize().y / 3);
 }
