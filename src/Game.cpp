00f7ca52b59f1503c44b75aa32e8f42e1f275a4b
NathanHaigh <nathan2.haigh@live.uwe.ac.uk>
2024-10-09 12:37:40 +0100

    First Code Input

--- src/Game.cpp
+++ src/Game.cpp
@@ -10,11 +10,22 @@ Game::Game(sf::RenderWindow& game_window)
 
 Game::~Game()
 {
-
+	delete [] animals;
+	delete[] passports;
+	delete character;
+	delete passport;
 }
 
 bool Game::init()
 {
+  character = new sf::Sprite;
+  passport = new sf::Sprite;
+  
+  if (!background_texture.loadFromFile("Data/Images/kenney_physicspack/PNG/Backgrounds/colored_land.png"))
+  {
+	  std::cout << "Background didn't load in \n";
+  }
+  background.setTexture(background_texture);
 
   return true;
 }
@@ -26,7 +37,7 @@ void Game::update(float dt)
 
 void Game::render()
 {
-
+	window.draw(background);
 }
 
 void Game::mouseClicked(sf::Event event)
@@ -40,6 +51,32 @@ void Game::mouseClicked(sf::Event event)
 void Game::keyPressed(sf::Event event)
 {
 
+}
+
+void Game::newAnimal()
+{
+	passport_accepted = false;
+	passport_rejected = false;
+
+	int animal_index = rand() % 3;
+	int passport_index = rand() % 3;
+
+	if (animal_index == passport_index)
+	{
+		should_accept = true;
+	}
+	else
+	{
+		should_accept = false;
+	}
+
+	character->setTexture(animals[animal_index], true);
+	character->setScale(1.8, 1.8);
+	character->setPosition(window.getSize().x / 2, window.getSize().y / 12);
+
+	passport->setTexture(animals[animal_index], true);
+	passport->setScale(0.6, 0.6);
+	passport->setPosition(window.getSize().x / 2, window.getSize().y / 3);
 }
 
 
--- src/Game.h
+++ src/Game.h
@@ -14,11 +14,23 @@ class Game
   void render();
   void mouseClicked(sf::Event event);
   void keyPressed(sf::Event event);
+  void newAnimal();
 
  private:
   sf::RenderWindow& window;
-  sf::Sprite ball;
-  sf::Texture ball_texture;
+
+  sf::Sprite* character;
+  sf::Sprite* passport;
+
+  sf::Texture* animals = new sf::Texture[3];
+  sf::Texture* passports = new sf::Texture[3];
+
+  sf::Sprite background;
+  sf::Texture background_texture;
+
+  bool should_accept;
+  bool passport_accepted;
+  bool passport_rejected;
 
 };
 
